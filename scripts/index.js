00e60c433fa6aaf4fd8070133e5295f8deef54ad
mishavlasiuk1 <fabionsa+help@gmail.com>
2015-12-17 14:21:14 +0000

    add new file

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,9 @@ $(function(){
         'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
         + 'Lviv' + '&APPID=85dd9ac97b8b9d0cbc5f814a5b96ffa6&cnt=3&units=metric' + '&lang=' + 'ua' + '&callback=?',
         function (data) {
-            $('#todayTemp').html(data.list[0].temp.day);
-            $('#tomorrowTemp').html(data.list[1].temp.day);
-            $('#aftertomorrowTemp').html(data.list[2].temp.day);
+            $('#todayTemp').html((data.list[0].temp.day));
+            $('#tomorrowTemp').html(Math.round(data.list[1].temp.day));
+            $('#aftertomorrowTemp').html(Math.round(data.list[2].temp.day));
         }
     );
 });
