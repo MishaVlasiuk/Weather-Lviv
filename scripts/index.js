a5fba40e48aedf7ca044c5df9a6491fe99ea7f8c
mishavlasiuk1 <fabionsa+help@gmail.com>
2015-12-20 19:43:28 +0000

    add new file

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
-$(function(){
-    $.getJSON(
-        'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-        + 'Lviv' + '&APPID=85dd9ac97b8b9d0cbc5f814a5b96ffa6&cnt=3&units=metric' + '&lang=' + 'ua' + '&callback=?',
-        function (data) {
-            $('#todayTemp').html((data.list[0].temp.day));
-            $('#tomorrowTemp').html(Math.round(data.list[1].temp.day));
-            $('#aftertomorrowTemp').html(Math.round(data.list[2].temp.day));
+$(function() { 
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=Lviv'+
+    '&units=metric&cnt=3&appid=b3628aa47ecbfaadb367ea4779b8a8c1',
+        function(data) {
+          $('#todayTemp').html(data. list[0].temp.day);
+          $('#tomorrowTemp').html(data. list[1].temp.day);
+          $('#aftertomorrowTemp').html(data. list[2].temp.day);
+          $('#TodayPressure').html(data. list[0].pressure);
+          $('#TomorrowPressure').html(data. list[1].pressure);
+          $('#AfterTomorrowPressure').html(data. list[2].pressure);
+          $('#iconToday').html('<img src="img/'+ data.list[0].weather[0].icon + '.png" alt="Weather icon">');
+          $('#iconTomorrow').html('<img src="img/'+ data.list[1].weather[0].icon + '.png" alt="Weather icon">');
+          $('#iconAfterTomorrow').html('<img src="img/'+ data.list[2].weather[0].icon + '.png" alt="Weather icon">');
         }
     );
 });
